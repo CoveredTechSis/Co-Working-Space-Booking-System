27d6cd54a10ce8f5b432a21b6b7ca6fb7dfdb1f0
CoveredTechSis <aishaolalekan@gmail.com>
2024-11-10 19:14:07 +0100

    added a styled heropage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import DeskGrid from './components/DeskGrid/DeskGrid'
+import HeroPage from './components/HeroPage/HeroPage'
 
 function App() {
   
 
   return (
     <>
+    <HeroPage/>
     <DeskGrid/>
     </>
   )
